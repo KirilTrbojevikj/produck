8a38436ac16720b9d803b4175a18fccbecdc5890
kiril <kiril.trbojevikj@gmail.com>
2022-07-31 10:49:59 +0200

    Start Feed Post UI

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:produck/screens/add_post_screen.dart';
+import 'package:produck/screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems=[
-  Text('feed'),
+  FeedScreen(),
   Text('search'),
   AddPostScreen(),
   Text('favourite'),
